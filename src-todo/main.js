237dd5e9e92883bffdd501725b5972ecf53cad8f
2804420942 <2804420942@qq.com>
2019-09-20 19:29:29 +0800

    第二次提交

--- src-todo/main.js
+++ src-todo/main.js
@@ -1,8 +1,15 @@
-/*
-入口JS
+/**
+ * 
+ * @authors ${author} (${email})
+ * @date    2019-09-19
+ * @version $Id$
  */
 import Vue from 'vue'
 import App from './App.vue'
+
+import './base.css'
+
+
 
 new Vue({
 	el: '#app',
